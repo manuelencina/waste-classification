b03c23e2e28887a5e9083de417e989e29dc813ac
0xCronos <56097041+0xCronos@users.noreply.github.com>
2023-07-12 09:24:02 -0400

    feature: Allows prediction module to save output video and results.

--- predict.py
+++ predict.py
@@ -1,22 +1,13 @@
-import cv2
-import os
 from ultralytics import YOLO
 
-# model = YOLO("yolov8n.pt")
-# model.predict("example2.mp4", show=True)
-
-def save_img_results(dir_name: str, fname: str):
-    fnames = os.listdir(dir_name)
-
-    for fname in fnames:
-        img = cv2.imread(f"{dir_name}/{fname}")
-        pred = model.predict(img)[0]
-        plot = pred.plot()
-        cv2.imwrite(f"{fname}/{fname}", plot)
 
 if __name__ == "__main__":
     model = YOLO("runs/detect/train/weights/best.pt")
-    dir_name = "datasets/test/images"
-    fname = "validation/images"
-    
-    save_img_results(dir_name, fname)
+    model.predict(
+        "videoname.mp4",
+        conf=0.6,
+        max_det=10,
+        device=0,
+        save=True,
+        show=True
+    )
--- train.py
+++ train.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     data_file = os.path.join(current_dir, "data.yaml")
     model = YOLO("yolov8n.pt")
 
-    model.train(data=data_file, epochs=10, imgsz=(416,416), batch=8, optimizer="Adam")
+    model.train(data=data_file, epochs=50, imgsz=640, batch=8, optimizer="Adam", device=0)
